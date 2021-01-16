daad8d188076e4c02e92a8f4e98813fbd524fad9
EDSON MATEUS ALVES DE OLIVEIRA <tioed2015@gmail.com>
2021-01-16 13:30:00 -0300

    salve

--- src/App.vue
+++ src/App.vue
@@ -5,24 +5,14 @@
 </template>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+*{
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
 }
 
-#nav {
-  padding: 30px;
+button:hover{
+  cursor: pointer;
 }
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
 </style>

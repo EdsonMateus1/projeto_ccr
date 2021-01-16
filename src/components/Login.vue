b1a02c62e16983ee5e7f4f0d9d21b97df132fadd
EDSON MATEUS ALVES DE OLIVEIRA <tioed2015@gmail.com>
2021-01-16 00:19:39 -0300

    comeco de tudo

---
+++ src/components/Login.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Login",
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped></style>

b04ff1ab03ec8b778464886350f1bb9d85aaac95
EDSON MATEUS ALVES DE OLIVEIRA <tioed2015@gmail.com>
2021-01-16 13:21:00 -0300

    router

---
+++ src/router/index.js
@@ -0,0 +1,19 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+
+
+Vue.use(VueRouter)
+
+const routes = [
+  {
+    path: '/',
+    name: 'Login',
+    component: () => import(/* webpackChunkName: "about" */ '../views/Sigin.vue')
+  },
+]
+
+const router = new VueRouter({
+  routes
+})
+
+export default router

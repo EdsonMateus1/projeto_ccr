5edfdbfbb8cdd551dd3460c654efec76226ac084
EDSON MATEUS ALVES DE OLIVEIRA <tioed2015@gmail.com>
2021-01-16 02:27:56 -0300

    ..

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -106,7 +106,6 @@ export default {
   margin-top: 30px;
   border-radius: 10px;
   background-color: #fe1243;
-
   color: #f5f5f5;
   font-size: 20px;
   box-shadow: 0px 0px 5px 0px rgba(0, 0, 0, 0.6);

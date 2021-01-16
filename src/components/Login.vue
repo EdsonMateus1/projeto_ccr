8c70ce11d80b6ff40adec8b791b1213aa51fdfd3
Pedro Lima <pedropplz77@gmail.com>
2021-01-16 13:04:09 -0300

    alter3

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -44,7 +44,7 @@
             d="M15.545 6.558a9.42 9.42 0 0 1 .139 1.626c0 2.434-.87 4.492-2.384 5.885h.002C11.978 15.292 10.158 16 8 16A8 8 0 1 1 8 0a7.689 7.689 0 0 1 5.352 2.082l-2.284 2.284A4.347 4.347 0 0 0 8 3.166c-2.087 0-3.86 1.408-4.492 3.304a4.792 4.792 0 0 0 0 3.063h.003c.635 1.893 2.405 3.301 4.492 3.301 1.078 0 2.004-.276 2.722-.764h-.003a3.702 3.702 0 0 0 1.599-2.431H8v-3.08h7.545z"
           />
         </svg>
-        <span class="span-access">Google</span>
+        <span class="span-access">Entre com a conta Google</span>
       </button>
       <button class="button-access">
         <svg
@@ -60,7 +60,7 @@
             d="M16 8.049c0-4.446-3.582-8.05-8-8.05C3.58 0-.002 3.603-.002 8.05c0 4.017 2.926 7.347 6.75 7.951v-5.625h-2.03V8.05H6.75V6.275c0-2.017 1.195-3.131 3.022-3.131.876 0 1.791.157 1.791.157v1.98h-1.009c-.993 0-1.303.621-1.303 1.258v1.51h2.218l-.354 2.326H9.25V16c3.824-.604 6.75-3.934 6.75-7.951z"
           />
         </svg>
-        <span class="span-access">Facebook</span>
+        <span class="span-access">Entre com o Facebook</span>
       </button>
     </div>
   </div>
@@ -88,7 +88,8 @@ export default {
   justify-content: flex-end;
   width: 100%;
   height: 90%;
-  margin: 30px 0;
+  margin-top: 20px;
+  margin-bottom: 40px;
 }
 
 .title {
@@ -107,6 +108,7 @@ export default {
   margin-bottom: 20px;
   color: #ccc;
   font-family: "Segoe UI", Tahoma, Geneva, Verdana, sans-serif;
+  opacity: 0.3;
 }
 
 .sub-title {

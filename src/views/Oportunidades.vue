1f5e8dcac6b60b27fc556b2143753aed5e608b8d
EDSON MATEUS ALVES DE OLIVEIRA <tioed2015@gmail.com>
2021-01-17 14:39:59 -0300

    oportunidade

--- src/views/Oportunidades.vue
+++ src/views/Oportunidades.vue
@@ -1,20 +1,7 @@
 <template>
-  <div class="oportunidades">
-    <Header />>
-    <h1>Escolha a area de preferencia abaixo</h1>
-    <h1>Escolha a area de preferencia abaixo</h1>
-    <Footer />
-  </div>
+  <div class="oportunidades"></div>
 </template>
 <script>
-import Header from "../components/Header";
-import Footer from "../components/Footer";
-
-export default {
-  components: {
-    Header,
-    Footer,
-  },
-};
+export default {};
 </script>
 <style></style>

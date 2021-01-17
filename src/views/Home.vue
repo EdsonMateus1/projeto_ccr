cb49b3e3beaedbf9a361cd22497d507449ed52c5
Pedro Lima <pedropplz77@gmail.com>
2021-01-16 22:50:31 -0300

    commit2250

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -29,21 +29,29 @@
         </div>
         <div class="card-conted">
           <h3 class="title-card">Grupos de estudos</h3>
-          <span class="description-card">Descubra bibliotexas para estudo,</span>
-          <span class="description-card">grupos para te incentivar a ir mais longe!</span>
+          <span class="description-card"
+            >Descubra bibliotexas para estudo,</span
+          >
+          <span class="description-card"
+            >grupos para te incentivar a ir mais longe!</span
+          >
         </div>
       </div>
 
       <div style="background-color: #670015" class="card">
         <div style="background-color: #800a21" class="card-header">
           <div class="container-img-card">
-            <img class="img-card" src="../assets/portfolio.png" />
+            <img class="img-card" src="../assets/Group_1.png" />
           </div>
         </div>
         <div class="card-conted">
           <h3 class="title-card">Vagas</h3>
-          <span class="description-card">Descubra vagas de emprego de acordo,</span>
-          <span class="description-card">com sue desempenho,você tera mais</span>
+          <span class="description-card"
+            >Descubra vagas de emprego de acordo,</span
+          >
+          <span class="description-card"
+            >com sue desempenho,você tera mais</span
+          >
           <span class="description-card">chances de ser entrevistado!</span>
         </div>
       </div>
@@ -56,17 +64,36 @@
         </div>
         <div class="card-conted">
           <h3 class="title-card">Projetos sociais</h3>
-          <span class="description-card">Pratique atividade fisicas,apoie causas e,</span>
+          <span class="description-card"
+            >Pratique atividade fisicas,apoie causas e,</span
+          >
           <span class="description-card">esteja em constante evolucao!</span>
         </div>
       </div>
     </div>
 
+    <section class="estudantes">
+      <div class="container">
+        <div class="grid-16 img-estudantes"></div>
+      </div>
+      <div class="subtitulo-curriculo">
+        <router-link class="subtitulo-curriculo" :to="{ name: 'Curriculo' }"
+          >Clique aqui
+        </router-link>
+        <h2>e publique suas notas!</h2>
+      </div>
+      <p>
+        "Daqui 20 ou 30 anos, as coisas que você não fez irão te decepcionar
+        muito mais do que aquelas que fez. <br />Portanto, corte as suas amarras
+        e explore o universo de possibilidades que lhe cercam."
+      </p>
+    </section>
+
     <div class="quebra">
       <blockquote class="quote-externo container">
         <p>
-          "Cada sonho que você deixa para trás é um pedaço do seu futuro que deixa de
-          existir."
+          "Cada sonho que você deixa para trás é um pedaço do seu futuro que
+          deixa de existir."
         </p>
         <cite>STEVE JOBS</cite>
       </blockquote>
@@ -78,8 +105,8 @@
           <div class="grid-8 footer_historia">
             <h3>Nossa História</h3>
             <p>
-              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ab ea, tempora
-              itaque laboriosam impedit tenetur.
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ab ea,
+              tempora itaque laboriosam impedit tenetur.
             </p>
           </div>
 
@@ -130,8 +157,6 @@ export default {
 </script>
 
 <style scoped>
-.home {
-}
 .container-img-central {
   background-image: url("../assets/Group_7.png");
   background-size: contain;
@@ -229,8 +254,48 @@ export default {
   .home {
     padding: 0px;
   }
-  .container-img-central {
-  }
+}
+
+.estudantes {
+  padding: 40px 0;
+}
+
+.estudantes {
+  background-image: url("../assets/estudantes.png");
+  background-repeat: no-repeat;
+  background-size: cover;
+  height: 700px;
+  width: 100%;
+}
+
+.subtitulo-curriculo {
+  z-index: 1;
+  color: #fe1243;
+  font-weight: bold;
+  font-size: 1.8em;
+  margin-top: 10.8%;
+  margin-left: 5%;
+  text-decoration: none;
+  display: flex;
+  flex-direction: row;
+  align-items: flex-start;
+}
+
+.subtitulo-curriculo h2 {
+  z-index: 1;
+  color: white;
+  font-weight: bold;
+  font-size: 1.8em;
+  margin-top: 10.8%;
+  margin-left: 0.8%;
+}
+
+.estudantes p {
+  color: white;
+  font-size: 0.5;
+  margin-top: 1%;
+  margin-left: 10%;
+  font-style: italic;
 }
 
 .quebra {
